875e16acea7397b99bf5030bdc268d5e3e5e5943
Muhaiminur Rahman <muhaiminurabir@gmail.com>
2019-07-16 10:55:49 +0600

    FINAL v1

---
+++ app/src/main/java/com/Jatiyo_tottho/nationalcallcenter/MainActivity.java
@@ -0,0 +1,474 @@
+package com.Jatiyo_tottho.nationalcallcenter;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.facebook.ads.Ad;
+import com.facebook.ads.AdError;
+import com.facebook.ads.AdIconView;
+import com.facebook.ads.AdListener;
+import com.facebook.ads.AdOptionsView;
+import com.facebook.ads.AdSize;
+import com.facebook.ads.AdView;
+import com.facebook.ads.AudienceNetworkAds;
+import com.facebook.ads.InterstitialAd;
+import com.facebook.ads.InterstitialAdListener;
+import com.facebook.ads.MediaView;
+import com.facebook.ads.NativeAd;
+import com.facebook.ads.NativeAdLayout;
+import com.facebook.ads.NativeAdListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.emergency)
+    Button emergency;
+    @BindView(R.id.info)
+    Button info;
+    @BindView(R.id.ambulance)
+    Button ambulance;
+    @BindView(R.id.durjog)
+    Button durjog;
+    @BindView(R.id.bank)
+    Button bank;
+    @BindView(R.id.law)
+    Button law;
+    @BindView(R.id.durniti)
+    Button durniti;
+    @BindView(R.id.manobhadikar)
+    Button manobhadikar;
+    @BindView(R.id.nirbachon)
+    Button nirbachon;
+    @BindView(R.id.nari1)
+    Button nari1;
+    @BindView(R.id.nari2)
+    Button nari2;
+    @BindView(R.id.krisi1)
+    Button krisi1;
+    @BindView(R.id.krisi2)
+    Button krisi2;
+    @BindView(R.id.btcl)
+    Button btcl;
+    @BindView(R.id.foreign)
+    Button foreign;
+    @BindView(R.id.family)
+    Button family;
+    @BindView(R.id.titas)
+    Button titas;
+
+
+    private AdView adView;
+    private InterstitialAd interstitialAd;
+
+    private final String TAG = MainActivity.class.getSimpleName();
+
+
+    private NativeAd nativeAd;
+    private NativeAdLayout nativeAdLayout;
+    private LinearLayout nativeadView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        AudienceNetworkAds.initialize(this);
+        bannerad();
+        interstellar();
+        loadNativeAd();
+    }
+
+    @OnClick({R.id.emergency, R.id.info, R.id.ambulance, R.id.durjog, R.id.bank, R.id.law, R.id.durniti, R.id.manobhadikar, R.id.nirbachon, R.id.nari1, R.id.nari2, R.id.krisi1, R.id.krisi2, R.id.btcl, R.id.foreign, R.id.family, R.id.titas})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.emergency:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_emergency_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.info:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_info_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.ambulance:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_ambulance_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.durjog:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_durjog_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.bank:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_bank_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.law:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_law_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.durniti:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_durniti_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.manobhadikar:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_manobadiker_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.nirbachon:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.nirbachon_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.nari1:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_nari_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.nari2:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_nari2_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.krisi1:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_krisi_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.krisi2:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_krisi2_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.btcl:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_btcl_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.foreign:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_foreign_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.family:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_family_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+            case R.id.titas:
+                try {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + getResources().getString(R.string.national_titas_code_string)));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Error Line Number", Log.getStackTraceString(e));
+                }
+                break;
+        }
+    }
+
+
+    public void bannerad() {
+        adView = new AdView(this, getResources().getString(R.string.homepage_banner), AdSize.BANNER_HEIGHT_50);
+
+        // Find the Ad Container
+        LinearLayout adContainer = findViewById(R.id.banner_container);
+
+        // Add the ad view to your activity layout
+        adContainer.addView(adView);
+
+
+        adView.setAdListener(new AdListener() {
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                // Ad error callback
+                /*Toast.makeText(MainActivity.this, "Error: " + adError.getErrorMessage(),
+                        Toast.LENGTH_LONG).show();*/
+                Log.d("Banner", "Error: " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Ad loaded callback
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Ad clicked callback
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Ad impression logged callback
+            }
+        });
+        // Request an ad
+        adView.loadAd();
+    }
+
+    private void interstellar() {
+        interstitialAd = new InterstitialAd(this, getResources().getString(R.string.homepage_intersteller));
+        // Set listeners for the Interstitial Ad
+        interstitialAd.setAdListener(new InterstitialAdListener() {
+            @Override
+            public void onInterstitialDisplayed(Ad ad) {
+                // Interstitial ad displayed callback
+                Log.e(TAG, "Interstitial ad displayed.");
+            }
+
+            @Override
+            public void onInterstitialDismissed(Ad ad) {
+                // Interstitial dismissed callback
+                Log.e(TAG, "Interstitial ad dismissed.");
+            }
+
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                // Ad error callback
+                Log.e(TAG, "Interstitial ad failed to load: " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Interstitial ad is loaded and ready to be displayed
+                Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!");
+                // Show the ad
+                //interstitialAd.show();
+                showAdWithDelay();
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Ad clicked callback
+                Log.d(TAG, "Interstitial ad clicked!");
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Ad impression logged callback
+                Log.d(TAG, "Interstitial ad impression logged!");
+            }
+        });
+
+        // For auto play video ads, it's recommended to load the ad
+        // at least 30 seconds before it is shown
+        interstitialAd.loadAd();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (adView != null) {
+            adView.destroy();
+        }
+
+        if (interstitialAd != null) {
+            interstitialAd.destroy();
+        }
+        super.onDestroy();
+    }
+
+    private void showAdWithDelay() {
+        /**
+         * Here is an example for displaying the ad with delay;
+         * Please do not copy the Handler into your project
+         */
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // Check if interstitialAd has been loaded successfully
+                if (interstitialAd == null || !interstitialAd.isAdLoaded()) {
+                    return;
+                }
+                // Check if ad is already expired or invalidated, and do not show ad if that is the case. You will not get paid to show an invalidated ad.
+                if (interstitialAd.isAdInvalidated()) {
+                    return;
+                }
+                // Show the ad
+                interstitialAd.show();
+            }
+        }, 10000); // Show the ad after 15 minutes
+    }
+
+
+    private void loadNativeAd() {
+        // Instantiate a NativeAd object.
+        // NOTE: the placement ID will eventually identify this as your App, you can ignore it for
+        // now, while you are testing and replace it later when you have signed up.
+        // While you are using this temporary code you will only get test ads and if you release
+        // your code like this to the Google Play your users will not receive ads (you will get a no fill error).
+        nativeAd = new NativeAd(this, getResources().getString(R.string.homepage_native));
+
+        nativeAd.setAdListener(new NativeAdListener() {
+            @Override
+            public void onMediaDownloaded(Ad ad) {
+                // Native ad finished downloading all assets
+                Log.e(TAG, "Native ad finished downloading all assets.");
+            }
+
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                // Native ad failed to load
+                Log.e(TAG, "Native ad failed to load: " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Native ad is loaded and ready to be displayed
+                Log.d(TAG, "Native ad is loaded and ready to be displayed!");
+                // Race condition, load() called again before last ad was displayed
+                if (nativeAd == null || nativeAd != ad) {
+                    return;
+                }
+                // Inflate Native Ad into Container
+                inflateAd(nativeAd);
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Native ad clicked
+                Log.d(TAG, "Native ad clicked!");
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Native ad impression
+                Log.d(TAG, "Native ad impression logged!");
+            }
+        });
+
+        // Request an ad
+        nativeAd.loadAd();
+    }
+
+    private void inflateAd(NativeAd nativeAd) {
+
+        nativeAd.unregisterView();
+
+        // Add the Ad view into the ad container.
+        nativeAdLayout = findViewById(R.id.native_ad_container);
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        // Inflate the Ad view.  The layout referenced should be the one you created in the last step.
+        nativeadView = (LinearLayout) inflater.inflate(R.layout.native_ad_layout, nativeAdLayout, false);
+        nativeAdLayout.addView(nativeadView);
+
+        // Add the AdOptionsView
+        LinearLayout adChoicesContainer = findViewById(R.id.ad_choices_container);
+        AdOptionsView adOptionsView = new AdOptionsView(MainActivity.this, nativeAd, nativeAdLayout);
+        adChoicesContainer.removeAllViews();
+        adChoicesContainer.addView(adOptionsView, 0);
+
+        // Create native UI using the ad metadata.
+        AdIconView nativeAdIcon = nativeadView.findViewById(R.id.native_ad_icon);
+        TextView nativeAdTitle = nativeadView.findViewById(R.id.native_ad_title);
+        MediaView nativeAdMedia = nativeadView.findViewById(R.id.native_ad_media);
+        TextView nativeAdSocialContext = nativeadView.findViewById(R.id.native_ad_social_context);
+        TextView nativeAdBody = nativeadView.findViewById(R.id.native_ad_body);
+        TextView sponsoredLabel = nativeadView.findViewById(R.id.native_ad_sponsored_label);
+        Button nativeAdCallToAction = nativeadView.findViewById(R.id.native_ad_call_to_action);
+
+        // Set the Text.
+        nativeAdTitle.setText(nativeAd.getAdvertiserName());
+        nativeAdBody.setText(nativeAd.getAdBodyText());
+        nativeAdSocialContext.setText(nativeAd.getAdSocialContext());
+        nativeAdCallToAction.setVisibility(nativeAd.hasCallToAction() ? View.VISIBLE : View.INVISIBLE);
+        nativeAdCallToAction.setText(nativeAd.getAdCallToAction());
+        sponsoredLabel.setText(nativeAd.getSponsoredTranslation());
+
+        // Create a list of clickable views
+        List<View> clickableViews = new ArrayList<>();
+        clickableViews.add(nativeAdTitle);
+        clickableViews.add(nativeAdCallToAction);
+
+        // Register the Title and CTA button to listen for clicks.
+        nativeAd.registerViewForInteraction(
+                nativeadView,
+                nativeAdMedia,
+                nativeAdIcon,
+                clickableViews);
+    }
+}
